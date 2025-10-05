971f7dac46d4c99b765aa602c5519be30dcada12
overma-cmd <overma@ivytech.edu>
2025-10-04 22:39:53 -0400

    Add files via upload

---
+++ raining.catsdogs-OasheenVerma.py
@@ -0,0 +1,15 @@
+# Program: It is raining cats and dogs.
+# Author: Oasheen Verma
+# Purpose: To print a sentence using variables
+
+# Variable to store the first animal
+c = "cats."
+
+# Variable to store the second animal
+d = "dogs."
+
+# Variable to store the full sentence
+s = "It is raining " + c[:-1] + " and " + d  # remove the period from cats and add it after dogs
+
+# Print the sentence
+print(s)
